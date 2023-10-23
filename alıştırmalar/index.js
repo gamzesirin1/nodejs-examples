81e21f3c563e07fd8ba7a7daec019554750bdd11
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-10-23 15:51:22 +0300

    added.

---
+++ alıştırmalar/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world</h1>')
+})
+
+app.get('/about', (req, res) => {
+	res.send('about me')
+})
+app.listen(port, () => {
+	console.log(`Server running on port ${port}.`)
+})
---
+++ alıştırmalar/index1.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const url = require("url");
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
